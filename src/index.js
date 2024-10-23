bd6ba1614b11beaa60cee7e10e55811180f2a3ad
jhonatan <jhonatancito0015@gmail.com>
2024-10-23 18:35:06 -0500

    mi primer commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const express = required('express');
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,resp)=>{
+app.get('/',(req,resp) => {
 
     res.send('hola mundo');
 });
- app.listen(port,()=>{
+ app.listen(port,() => {
 
     console.log(`servidor corriendo en http://localhost:${port}`)
  })
